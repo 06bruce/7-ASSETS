105d1952d94f20c41f9acd9596c59c502aae45c6
bruce06 <brucenshuti2@gmail.com>
2024-06-19 07:09:14 +0200

    Tried DB

---
+++ insert.php
@@ -0,0 +1,35 @@
+<!-- <!-- <?php
+
+echo{ 
+    <data>
+    data <Sign-up class="html"></Sign-up>{
+        First name:[],
+        Last Name:[]
+        Student Card Number:[]
+        Email:[]
+
+    }
+    </data>
+} -->
+
+
+
+<?php
+connection{
+    conn->insert.php("sign-up.html")
+}
+if connection be sucess full code tobe excutea;
+conn{
+    <i class="fa fa-external-link-square" aria-hidden="true"></i>
+}
+else connection
+
+
+
+
+
+
+
+
+
+_check_syntax -->
